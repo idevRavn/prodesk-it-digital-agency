0681007ef65ec760f6b0e9d4a7bbd0e99446a5e3
idevRavn <idevravn@gmail.com>
2026-03-13 16:11:15 +0530

    feat: add dark mode toggle button

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+// Dark mode toggle
+const toggle = document.getElementById("theme-toggle");
+
+toggle.addEventListener("click", () => {
+  const html = document.documentElement;
+
+  if (html.dataset.theme === "dark") {
+    html.dataset.theme = "light";
+    toggle.textContent = "🌚";
+  } else {
+    html.dataset.theme = "dark";
+    toggle.textContent = "🌞";
+  }
+});
